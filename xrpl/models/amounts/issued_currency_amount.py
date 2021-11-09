086bd5cdee06143848f34d28d3f898dcd252fb83
Mayukha Vadari <mvadari@ripple.com>
2021-11-09 12:36:31 -0500

    add from_issued_currency

--- xrpl/models/amounts/issued_currency_amount.py
+++ xrpl/models/amounts/issued_currency_amount.py
@@ -3,9 +3,13 @@ Specifies an amount in an issued currency.
 
 See https://xrpl.org/currency-formats.html#issued-currency-amounts.
 """
+from __future__ import annotations
+
 from dataclasses import dataclass
+from typing import Type, Union, cast
 
 from xrpl.models.currencies.issued_currency import IssuedCurrency
+from xrpl.models.currencies.xrp import XRP
 from xrpl.models.required import REQUIRED
 from xrpl.models.utils import require_kwargs_on_init
 
@@ -25,3 +29,25 @@ class IssuedCurrencyAmount(IssuedCurrency):
 
     :meta hide-value:
     """
+
+    @classmethod
+    def from_issued_currency(
+        cls: Type[IssuedCurrencyAmount], issued_currency: IssuedCurrency, value: str
+    ) -> Union[IssuedCurrencyAmount, str]:
+        """
+        Build an IssuedCurrencyAmount from an IssuedCurrency.
+
+        Args:
+            issued_currency: The issued currency to use.
+            value: The amount of issued currency.
+
+        Returns:
+            An IssuedCurrencyAmount with the provided value in the provided issued
+            currency.
+        """
+        if isinstance(issued_currency, XRP):
+            return value
+        return cast(
+            IssuedCurrencyAmount,
+            cls.from_dict({**issued_currency.to_dict(), "value": value}),
+        )
