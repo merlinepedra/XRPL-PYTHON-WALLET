0f72d6f436ff652d74768e0f86992048dbccff6e
Mayukha Vadari <mvadari@ripple.com>
2021-12-01 16:10:01 -0500

    add json test

--- tests/integration/reqs/test_account_info.py
+++ tests/integration/reqs/test_account_info.py
@@ -1,6 +1,7 @@
 from tests.integration.integration_test_case import IntegrationTestCase
 from tests.integration.it_utils import test_async_and_sync
 from tests.integration.reusable_values import WALLET
+from xrpl.asyncio.clients.json_rpc_base import JsonRpcBase
 from xrpl.models.requests import AccountInfo
 
 
@@ -13,3 +14,24 @@ class TestAccountInfo(IntegrationTestCase):
             )
         )
         self.assertTrue(response.is_successful())
+
+    @test_async_and_sync(globals())
+    async def test_basic_functionality_json(self, client):
+        http = {
+            "method": "account_info",
+            "params": [
+                {
+                    "account": WALLET.classic_address,
+                }
+            ],
+        }
+        ws = {"command": "account_info", "account": WALLET.classic_address}
+        if isinstance(client, JsonRpcBase):
+            jsn = http
+        else:
+            jsn = ws
+        response = await client.request_json(jsn)
+        if isinstance(client, JsonRpcBase):
+            self.assertEqual(response["result"]["status"], "success")
+        else:
+            self.assertEqual(response["status"], "success")
